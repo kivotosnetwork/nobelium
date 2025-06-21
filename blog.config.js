7b4df0c8da51464c4e64bc3f5a52e68160fe861e
Tasuku Hachisuka <wmzwsa@gmail.com>
2025-06-21 19:13:11 +0800

    Update.

--- blog.config.js
+++ blog.config.js
@@ -1,9 +1,9 @@
 const BLOG = {
-  title: 'Wlog',
-  author: 'Tasuku Hachisuka',
-  email: 't@sk.ci',
-  link: 'https://blog.wmz1024.com',
-  description: 'Tasuku\'s Blog',
+  title: '基沃网路',
+  author: '基沃网路',
+  email: 'kivotos@horzteam.com',
+  link: 'https://minecraft.kivotos.net',
+  description: 'Kivotos Network 基沃网路 ｜ RPG生存工业建筑Minecraft服务器',
   lang: 'zh-CN', // ['en-US', 'zh-CN', 'zh-HK', 'zh-TW', 'ja-JP', 'es-ES']
   timezone: 'Asia/Shanghai', // Your Notion posts' date will be interpreted as this timezone. See https://en.wikipedia.org/wiki/List_of_tz_database_time_zones for all options.
   appearance: 'auto', // ['light', 'dark', 'auto'],
@@ -11,29 +11,29 @@ const BLOG = {
   lightBackground: '#ffffff', // use hex value, don't forget '#' e.g #fffefc
   darkBackground: '#18181B', // use hex value, don't forget '#'
   path: '', // leave this empty unless you want to deploy Nobelium in a folder
-  since: 2020, // If leave this empty, current year will be used.
+  since: 2025, // If leave this empty, current year will be used.
   postsPerPage: 7,
   sortByDate: true,
   showAbout: true,
   showArchive: true,
   autoCollapsedNavBar: false, // The automatically collapsed navigation bar
   ogImageGenerateURL: 'https://og-image-craigary.vercel.app', // The link to generate OG image, don't end with a slash
-  socialLink: 'https://www.wmz1024.com/',
+  socialLink: 'https://kivotos.net/',
   seo: {
     keywords: ['Blog', 'Website', 'Tasuku','Hachisuka',"Wlog"],
     googleSiteVerification: '' // Remove the value or replace it with your own google site verification code
   },
-  notionPageId: process.env.NOTION_PAGE_ID, // DO NOT CHANGE THIS！！！
+  notionPageId: "219fdea4db5c809d862bcdd5e83f3f92", // DO NOT CHANGE THIS！！！
   notionAccessToken: process.env.NOTION_ACCESS_TOKEN, // Useful if you prefer not to make your database public
   analytics: {
-    provider: 'ga', // Currently we support Google Analytics and Ackee, please fill with 'ga' or 'ackee', leave it empty to disable it.
+    provider: '', // Currently we support Google Analytics and Ackee, please fill with 'ga' or 'ackee', leave it empty to disable it.
     ackeeConfig: {
       tracker: '', // e.g 'https://ackee.craigary.net/tracker.js'
       dataAckeeServer: '', // e.g https://ackee.craigary.net , don't end with a slash
       domainId: '' // e.g '0e2257a8-54d4-4847-91a1-0311ea48cc7b'
     },
     gaConfig: {
-      measurementId: 'G-5687YQS5LG' // e.g: G-XXXXXXXXXX
+      measurementId: '' // e.g: G-XXXXXXXXXX
     }
   },
   comment: {
